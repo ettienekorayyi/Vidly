4b7b6eb2fe53301336b031aae1c7641b89cf29f5
stv <corralstephenmelben@gmail.com>
2019-05-10 23:51:55 +1000

    Initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        /// <summary>
+        /// 1.SingleOrDefault()
+        /// reference: https://stackoverflow.com/questions/23842998/the-model-item-passed-into-the-dictionary-is-of-type-system-linq-enumerable
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            // 1.SingleOrDefault()
+            // Without getting the first element of the list you are actually 
+            // passing an enumerable list instead of a single item.
+            var query = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            //if (query == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            
+            return View(query);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        { 
+            return new List<Customer>() 
+            {
+               new Customer { Id = 1, Name = "John Smith" },
+               new Customer { Id = 2, Name = "Mary Jane" },
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        [Route("movie/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var query = GetMovies().SingleOrDefault(m => m.Id == id);
+            //if (query == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            // 1.SingleOrDefault()
+            // Without getting the first element of the list you are actually 
+            // passing an enumerable list instead of a single item.
+            return View(query);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        // Can be overriden by using query string
+        // Cannot be overridden by using url because 
+        // that would require you to create a custom route.
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>() 
+            {
+               new Movie { Id = 1, Name = "Shrek" },
+               new Movie { Id = 2, Name = "Wall-E" },
+            };
+        }
+    }
+}
